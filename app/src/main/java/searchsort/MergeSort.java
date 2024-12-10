69e8cf5e2ee63e498a83f1a4e742822d1df5c886
polo2325 <flipflop2325@outlook.com>
2024-12-09 21:35:28 -0500

    merge

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -12,7 +12,12 @@ public class MergeSort extends Sort {
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
+<<<<<<< HEAD
     private void merge(int[] arr, int[] left, int[] right) {
         
+=======
+    private void merge(int[] arr, int[] aux, int lo, int mid, int hi) {
+
+>>>>>>> 5b66f383b73d18e974829135bcb26aebc497cc1e
     }
 }
