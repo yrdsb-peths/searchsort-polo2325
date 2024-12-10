a1806c7426dc7563fce303fc2638b69c0c880685
polo2325 <flipflop2325@outlook.com>
2024-12-09 21:42:19 -0500

    merge

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,18 +6,38 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
+        if(arr.length<=1) {
+
+        }
+
+        // merge()
 
     }
 
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
-<<<<<<< HEAD
-    private void merge(int[] arr, int[] left, int[] right) {
-        
-=======
     private void merge(int[] arr, int[] aux, int lo, int mid, int hi) {
-
->>>>>>> 5b66f383b73d18e974829135bcb26aebc497cc1e
+        mergeHelp(arr, )
     }
+
+    void mergeHelp(int[] arr, int[] aux, int lo, int mid, int hi){
+     
+        for(int k = lo; k <= hi; k++)
+            aux[k] = arr[k];
+      
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                arr[k] = aux[j++];
+            } else if (j > hi) {
+                arr[k] = aux[i++];
+            } else if (aux[j] < aux[i]) {
+                arr[k] = aux[j++];
+            } else {
+                arr[k] = aux[i++];
+            }                       
+        }
+    }   
 }
